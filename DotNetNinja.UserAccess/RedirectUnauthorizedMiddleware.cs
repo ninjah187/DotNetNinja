b0aabbad3c9f2bca274603c1d4661ead324ea6fa
Karol Hinz <k.hinz@intuiti.software>
2016-08-23 15:55:34 +0200

    works on unauthorized responses redirecting

---
+++ DotNetNinja.UserAccess/RedirectUnauthorizedMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace DotNetNinja.UserAccess
+{
+    public class RedirectUnauthorizedMiddleware
+    {
+        readonly RequestDelegate _next;
+        RedirectUnauthorizedOptions _options;
+
+        public RedirectUnauthorizedMiddleware(RequestDelegate next, RedirectUnauthorizedOptions options)
+        {
+            _next = next;
+            _options = options;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+
+            if (context.Response.StatusCode == 401)
+            {
+                context.Response
+            }
+        }
+    }
+}
---
+++ DotNetNinja.UserAccess/RedirectUnauthorizedOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetNinja.UserAccess
+{
+    public class RedirectUnauthorizedOptions
+    {
+        public string RouteValue { get; set; }
+    }
+}
