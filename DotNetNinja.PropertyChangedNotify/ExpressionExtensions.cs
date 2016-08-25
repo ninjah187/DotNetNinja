0d310bdb48c2ac428d7cf4181de1c5cfde43e8c6
Karol Hinz <k.hinz@intuiti.software>
2016-08-25 10:36:59 +0200

    tweaks on PropertyChangedNotify

---
+++ DotNetNinja.PropertyChangedNotify/ExpressionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DotNetNinja.PropertyChangedNotify
+{
+    public static class ExpressionExtensions
+    {
+        /// <summary>
+        /// Extracts property name from Expression if its body is MemberExpression, otherwise throws ArgumentException.
+        /// </summary>
+        /// <param name="expr"></param>
+        /// <returns></returns>
+        public static string ExtractPropertyName<TProperty>(this Expression<TProperty> expr)
+        {
+            var memberExpression = expr.Body as MemberExpression;
+
+            if (memberExpression == null)
+            {
+                throw new ArgumentException($"Expression's body of {nameof(expr)} must be a {nameof(MemberExpression)}", nameof(expr));
+            }
+
+            return memberExpression.Member.Name;
+        }
+    }
+}
--- DotNetNinja.PropertyChangedNotify/PropertyChangedNotifier.cs
+++ DotNetNinja.PropertyChangedNotify/PropertyChangedNotifier.cs
@@ -1,14 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace DotNetNinja.PropertyChangedNotify
 {
-    public class Class1
+    /// <summary>
+    /// Base class for objects that notify properties changes.
+    /// </summary>
+    public class PropertyChangedNotifier : INotifyPropertyChanged
     {
-        public Class1()
+        /// <summary>
+        /// Event indicating that a property is changed on a component.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises PropertyChanged event.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        /// <summary>
+        /// Sets value of property and raises PropertyChanged event if the property has changed.
+        /// </summary>
+        /// <typeparam name="T">Property type.</typeparam>
+        /// <param name="backingField">Field that backs target property.</param>
+        /// <param name="value">Target value.</param>
+        /// <param name="propertyExpression">Expression describing property.</param>
+        /// <returns>Value indicating whether property value has changed (therefore PropertyChanged was raised).</returns>
+        protected bool SetProperty<T>(ref T backingField, T value, Expression<Func<T>> propertyExpression)
+            => SetProperty(ref backingField, value, propertyExpression.ExtractPropertyName());
+
+        /// <summary>
+        /// Sets value of property and raises PropertyChanged event if the property has changed.
+        /// </summary>
+        /// <typeparam name="T">Property type.</typeparam>
+        /// <param name="backingField">Field that backs target property.</param>
+        /// <param name="value">Target value.</param>
+        /// <param name="propName">Property name.</param>
+        /// <returns>Value indicating whether property value has changed (therefore PropertyChanged was raised).</returns>
+        protected bool SetProperty<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
         {
+            var changed = !EqualityComparer<T>.Default.Equals(backingField, value);
+
+            if (changed)
+            {
+                backingField = value;
+                OnPropertyChanged(propertyName);
+            }
+
+            return changed;
         }
     }
 }
