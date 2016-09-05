47d6734e9c1ac5f7d841fb3526f1c756f5ecc127
Karol Hinz <k.hinz@intuiti.software>
2016-09-05 13:27:04 +0200

    DotNetNinja.Wpf.Command added

---
+++ DotNetNinja.Wpf.Command/DotNetNinja.Wpf.Command/Command.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DotNetNinja.Wpf.Command
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        protected Action What { get; set; }
+        protected Func<bool> When { get; set; }
+
+        public Command(Action what)
+            : this(what, () => true)
+        {
+        }
+
+        public Command(Action what, Func<bool> when)
+        {
+            if (what == null)
+            {
+                throw new ArgumentNullException(nameof(what));
+            }
+
+            if (when == null)
+            {
+                throw new ArgumentNullException(nameof(when));
+            }
+
+            What = what;
+            When = when;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return When();
+        }
+
+        public void Execute(object parameter)
+        {
+            What();
+        }
+    }
+}
---
+++ DotNetNinja.Wpf.Command/DotNetNinja.Wpf.Command/CommandParametrized.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DotNetNinja.Wpf.Command
+{
+    public class CommandParametrized<TParameter> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        protected Action<TParameter> What { get; set; }
+        protected Func<TParameter, bool> When { get; set; }
+
+        public CommandParametrized(Action<TParameter> what)
+            : this(what, x => true)
+        {
+        }
+
+        public CommandParametrized(Action<TParameter> what, Func<TParameter, bool> when)
+        {
+            if (what == null)
+            {
+                throw new ArgumentNullException(nameof(what));
+            }
+
+            if (when == null)
+            {
+                throw new ArgumentNullException(nameof(when));
+            }
+
+            What = what;
+            When = when;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return When((TParameter) parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            What((TParameter) parameter);
+        }
+    }
+}
