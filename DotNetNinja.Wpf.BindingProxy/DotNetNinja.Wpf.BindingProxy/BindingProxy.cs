f74379d25ec783f129f763012fb188add9e059b6
Karol Hinz <k.hinz@intuiti.software>
2016-09-06 09:03:37 +0200

    DotNetNinja.Wpf.BindingProxy addded

---
+++ DotNetNinja.Wpf.BindingProxy/DotNetNinja.Wpf.BindingProxy/BindingProxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DotNetNinja.Wpf.BindingProxy
+{
+    public class BindingProxy : Freezable
+    {
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+
+        public object Data
+        {
+            get { return GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+    }
+}
