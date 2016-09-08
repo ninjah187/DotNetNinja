e3bcab168aa2b8e9e09120a6ac02e96a72900c30
karolhnz <karolhnz@gmail.com>
2016-09-08 18:38:26 +0200

    first ImageResizer test set

---
+++ DotNetNinja.AspNetCore.ImageResizer/DotNetNinja.AspNetCore.ImageResizer.Tests/ImagePathGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using DotNetNinja.AspNetCore.ImageResizer;
+
+namespace DotNetNinja.AspNetCore.ImageResizer.Tests
+{
+    public class ImagePathGeneratorTest
+    {
+        public static IEnumerable<object> GenerateResizedPathData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { "image.jpg", new Size { Width = 100, Height = 100 }, "image-100x100.jpg" }
+                };
+            }
+        }
+
+        [Theory, MemberData(nameof(GenerateResizedPathData))]
+        public void GenerateResizedPath(string inputPath, Size desiredSize, string expectedOutput)
+        {
+            var generator = new ImagePathGenerator();
+
+            var output = generator.GetResizedPath(inputPath, desiredSize);
+
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
